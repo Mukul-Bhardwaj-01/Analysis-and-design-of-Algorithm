95d65625de162dc6510d50cfb598024182cf9b4b
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-04-30 08:03:11 +0530

    Update Strassen multiplication.cpp

--- Lab 5 (27 February 2025)/Strassen multiplication.cpp
+++ Lab 5 (27 February 2025)/Strassen multiplication.cpp
@@ -1,57 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void addMatrix(int A[2][2], int B[2][2], int C[2][2])
-{
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            C[i][j] = A[i][j] + B[i][j];
-        }
-    }
-}
-
-void subtractMatrix(int A[2][2], int B[2][2], int C[2][2])
-{
-    for (int i = 0; i < 2; i++)
-    {   for (int j = 0; j < 2; j++)
-        {   
-            C[i][j] = A[i][j] - B[i][j];
-        }
-    }
-}
-
 void strassenMultiply(int A[2][2], int B[2][2], int C[2][2])
 {
-    int M1[2][2], M2[2][2], M3[2][2], M4[2][2], M5[2][2], M6[2][2], M7[2][2];
-    int temp1[2][2], temp2[2][2];
+    int M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    int M2 = (A[1][0] + A[1][1]) * B[0][0];
+    int M3 = A[0][0] * (B[0][1] - B[1][1]);
+    int M4 = A[1][1] * (B[1][0] - B[0][0]);
+    int M5 = (A[0][0] + A[0][1]) * B[1][1];
+    int M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    int M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
 
-    addMatrix(A, A, temp1);
-    addMatrix(B, B, temp2);
-    strassenMultiply(temp1, temp2, M1);
-    addMatrix(A, A, temp1);
-    strassenMultiply(temp1, B, M2);
-    subtractMatrix(B, B, temp1);
-    strassenMultiply(A, temp1, M3);
-    subtractMatrix(B, B, temp1);
-    strassenMultiply(A, temp1, M4);
-    addMatrix(A, A, temp1);
-    strassenMultiply(temp1, B, M5);
-    subtractMatrix(A, A, temp1);
-    addMatrix(B, B, temp2);
-    strassenMultiply(temp1, temp2, M6);
-    subtractMatrix(A, A, temp1);
-    addMatrix(B, B, temp2);
-    strassenMultiply(temp1, temp2, M7);
-    addMatrix(M1, M4, temp1);
-    subtractMatrix(temp1, M5, temp2);
-    addMatrix(temp2, M7, C);
-    addMatrix(M3, M5, C);
-    addMatrix(M2, M4, C);
-    addMatrix(M1, M3, temp1);
-    subtractMatrix(temp1, M2, temp2);
-    addMatrix(temp2, M6, C);
+    C[0][0] = M1 + M4 - M5 + M7;
+    C[0][1] = M3 + M5;
+    C[1][0] = M2 + M4;
+    C[1][1] = M1 - M2 + M3 + M6;
 }
 
 int main()
@@ -60,30 +23,23 @@ int main()
 
     cout << "Enter elements of first 2x2 matrix:\n";
     for (int i = 0; i < 2; i++)
-    {
         for (int j = 0; j < 2; j++)
-        {
             cin >> A[i][j];
-        }    
-    }
+
     cout << "Enter elements of second 2x2 matrix:\n";
     for (int i = 0; i < 2; i++)
-    {   
         for (int j = 0; j < 2; j++)
-        {
             cin >> B[i][j];
-        }
-    }
+
     strassenMultiply(A, B, C);
 
     cout << "Resultant matrix after Strassen's multiplication:\n";
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
-        {    
             cout << C[i][j] << " ";
-        }
         cout << endl;
     }
+
     return 0;
 }
